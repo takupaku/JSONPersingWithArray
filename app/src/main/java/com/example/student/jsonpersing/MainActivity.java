822d40088f413a9f88ccf2bb105c5f419bbee84e
taku paku <takupaku@yahoo.com>
2018-10-14 14:21:54 +0600

    if array was used instead of object

--- app/src/main/java/com/example/student/jsonpersing/MainActivity.java
+++ app/src/main/java/com/example/student/jsonpersing/MainActivity.java
@@ -10,6 +10,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -19,7 +20,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private String URL ="https://api.myjson.com/bins/1d9uo0";
+  // private String URL = "https://api.myjson.com/bins/1d9uo0";
+    private String nURL = "https://api.myjson.com/bins/amti8";
     RequestQueue requestQueue;
     TextView tvResult;
 
@@ -32,52 +34,98 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        tvResult=findViewById(R.id.tvResult);
+        tvResult = findViewById(R.id.tvResult);
     }
 
     private void initVariable() {
-        requestQueue= Volley.newRequestQueue(this);
+        requestQueue = Volley.newRequestQueue(this);
 
     }
 
     public void getJSONDATA(View view) {
-        final StringBuffer stringBuffer= new StringBuffer();
+        final StringBuffer stringBuffer = new StringBuffer();
 
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, nURL, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONArray jsonArray = response.getJSONArray("student");
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject std = (JSONObject) jsonArray.get(i);
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject std = response.getJSONObject(i);
+
 
                         stringBuffer.append(std.getString("id")).append(" . ").append(std.getString("name"))
                                 .append(" . ").append(std.getString("dept")).append("\n\n");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                    tvResult.setText(stringBuffer);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+
                 }
+                tvResult.setText(stringBuffer);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
-//        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
-//            @Override//response is holding the json api data
-//            public void onResponse(String response) {
+
+
+        requestQueue.add(jsonArrayRequest);
+
+
 //
-//                JSONObject jsonObject = response.get
+//        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                try {
+//                    JSONArray jsonArray = response.getJSONArray("student");
+//                    for (int i = 0; i < jsonArray.length(); i++) {
+//                        JSONObject std = (JSONObject) jsonArray.get(i);
+//
+//                        stringBuffer.append(std.getString("id")).append(" . ").append(std.getString("name"))
+//                                .append(" . ").append(std.getString("dept")).append("\n\n");
+//                    }
+//                    tvResult.setText(stringBuffer);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
 //            }
 //        }, new Response.ErrorListener() {
 //            @Override
 //            public void onErrorResponse(VolleyError error) {
-//
+//                Toast.makeText(MainActivity.this, "" + error.getMessage(), Toast.LENGTH_SHORT).show();
 //            }
-//        });//cntrl+spaace
-        requestQueue.add(request);
+//        });
+//cntrl+spaace
+        //requestQueue.add(request);
     }
 }
+
+
+/*
+in jsonlint
+
+
+{"student": [
+
+          {
+"id" :"32",
+"name" : "taku",
+"dept" : "cse"
+},
+{
+"id" :"32",
+"name" : "taku",
+"dept" : "cse"
+},{
+"id" :"32",
+"name" : "taku",
+"dept" : "cse"
+}
+]
+}
+
+----->then in myJson, api link is collected
+
+
+ */
